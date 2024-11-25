531bd5a5eb2f8d7693bb62165a3c481675f4feab
jinyoungjo <69689271+jinyoungjo@users.noreply.github.com>
2024-11-24 19:15:20 -0800

    Add files via upload

--- human_AI/experiment.js
+++ human_AI/experiment.js
@@ -56,5 +56,18 @@
         }
         timeline.push(block1);
 
+        var survey = {
+            type: jsPsychSurveyText,
+            questions: [
+              {prompt: 'What is your age?', name: 'age'},
+              {prompt: 'What is your gender?', name: 'gender'},
+              {prompt: 'What do you think the purpose of the experiment is?', name: 'purpose'},
+              {prompt: 'How many of the statements you were presented with did you already know to be true or false?', name: 'already_known'}
+            ]
+        };
+          
+          
+        
+        timeline.push(survey);
 
         jsPsych.run(timeline)
